62706ff4dde9939599970d695c28765bac1fde97
Théo <theo.klein@edu.esiee-it.fr>
2024-01-17 18:55:59 +0100

    client can send int value to server

--- client.c
+++ client.c
@@ -1,5 +1,24 @@
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
 int main(int argc, char const *argv[])
 {
-	/* code */
+	char message[] = "Hello World!";
+
+	union sigval value;
+	int pid = atoi(argv[1]);
+
+	value.sival_int = 42;
+
+	if(sigqueue(pid, SIGUSR1, value) == 0)
+		printf("Message send successfully !\n");
+	else
+		printf("Message failed\n");
+	
 	return 0;
 }
--- server.c
+++ server.c
@@ -1,5 +1,37 @@
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
+
+
+void startup()
+{
+	printf("Miniteams starting...\n");
+	printf("My PID is %i\n", getpid());
+	printf("Waiting for new messages\n");
+}
+
+
+
+void sig_handler(int sig, siginfo_t* info, void* vp)
+{
+	printf("received: %i\n", info->si_value.sival_int);
+}
+
+
 int main(int argc, char const *argv[])
 {
-	/* code */
+	startup();
+	struct sigaction sa;
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = sig_handler;
+
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+
+	while(1)
+	{
+		sleep(10);
+	}
 	return 0;
 }
+
