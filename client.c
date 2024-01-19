e6b87899913ad5dff7230c807ca5a215f1d9ecd7
Théo <theo.klein@edu.esiee-it.fr>
2024-01-19 18:02:28 +0100

    working

--- client.c
+++ client.c
@@ -2,41 +2,52 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
-void parser(int *parsed_message, char *message, size_t len)
+void parser(int *parsed_message, char *message, int len)
 {
 	for (int i = 0; i < len; i++)
 	{
 		parsed_message[i] = message[i];
 	}
-	parsed_message[len-1] = len;
+	parsed_message[len] = len;
+	parsed_message[len + 1] = '\0';
 
 }
 
 
 int main(int argc, char const *argv[])
 {
-	char message[] = "Hello World";
+	int len = strlen(argv[2]);
 
-	size_t len = sizeof(message)/sizeof(char);
+	printf("%i\n", len);
+
+	char message[len];
 	
-	int new_message[len + 1];
-	printf("HERE\n");
-
-	parser(new_message, message, len);
-	printf("HERE 2\n");
-
-	for (int i = 0; i < len + 1; i++)
+	if (len >= 1000)
 	{
-		union sigval value;
-		int pid = atoi(argv[1]);
+    	fprintf(stderr, "message is too long!\n");
+    	printf("HERE\n");
+    }
+    else
+    {
+    	strcpy(message, argv[2]);
 
-		value.sival_int = new_message[i];
-		printf("%i\n", new_message[i]);
-		if(sigqueue(pid, SIGUSR1, value) != 0)
-			printf("Message failed\n");
-		sleep(1);
+		int new_message[len + 2];
+	
+		parser(new_message, message, len);
+	
+		for (int i = 0; i < len + 2; i++)
+		{
+			union sigval value;
+			int pid = atoi(argv[1]);
+	
+			value.sival_int = new_message[i];
+			if(sigqueue(pid, SIGUSR1, value) != 0)
+				printf("Message failed\n");
+			sleep(0.01);
+		}
 	}
 
 	return 0;
--- server.c
+++ server.c
@@ -1,50 +1,48 @@
 #include <signal.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int buffer[251];
+int *buffer;
 
 void startup()
 {
 	printf("Miniteams starting...\n");
 	printf("My PID is %i\n", getpid());
 	printf("Waiting for new messages\n");
-	buffer[250] = -1;
+	buffer = malloc(1001 * sizeof *buffer);
+	buffer[1000] = -1;
 }
 
 void parser(int len)
 {
-	printf("HERE\n");
-	printf("%i\n", len);
-	int i = 0;
-	char message[len];
-	for (int i = 0; i < len; ++i)
+	char message[len + 1];
+
+	for (int i = 0; i < len + 1; i++)
 	{
-		message[i] = buffer[i];
-		printf("%c\n", message[i]);
+		message[i] = *(buffer + i);
 	}
 
-	printf("%s\n", message);
+	message[len] = '\0';
+
+	printf("%s \n", message);
 }
 
 void sig_handler(int sig, siginfo_t* info, void* vp)
 {
-	printf("received: %i\n", info->si_value.sival_int);
-
 	int character = info->si_value.sival_int;
 
-	if (*buffer != -1)
-		if (character != '\0')
-		{
-			*buffer = character;
-			printf("%i\n", *buffer);
-			buffer+sizeof(int);
-		}
-		else
-		{
-			parser(*(buffer-(2 *sizeof(int))));
-			buffer - (*(buffer) + 1) * sizeof(int);
-		}
+	*buffer = character;
+	
+	if (*buffer == '\0')
+	{
+		int len = *(buffer - 1);
+		buffer -= len + 1;
+		parser(len);
+		
+	}	
+	else
+		buffer++;
 }
 
 int main(int argc, char const *argv[])
@@ -58,8 +56,10 @@ int main(int argc, char const *argv[])
 
 	while(1)
 	{
-		sleep(10);
+		pause();
 	}
+
+	free(buffer);
 	return 0;
 }
 
