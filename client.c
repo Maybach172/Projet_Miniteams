b855ccfff49d4ba14d547b505afc97ef8d4aa8cd
Théo <theo.klein@edu.esiee-it.fr>
2024-01-19 10:11:49 +0100

    almost there

--- client.c
+++ client.c
@@ -4,21 +4,40 @@
 #include <stdlib.h>
 
 
+void parser(int *parsed_message, char *message, size_t len)
+{
+	for (int i = 0; i < len; i++)
+	{
+		parsed_message[i] = message[i];
+	}
+	parsed_message[len-1] = len;
+
+}
 
 
 int main(int argc, char const *argv[])
 {
-	char message[] = "Hello World!";
+	char message[] = "Hello World";
 
-	union sigval value;
-	int pid = atoi(argv[1]);
-
-	value.sival_int = 42;
-
-	if(sigqueue(pid, SIGUSR1, value) == 0)
-		printf("Message send successfully !\n");
-	else
-		printf("Message failed\n");
+	size_t len = sizeof(message)/sizeof(char);
 	
+	int new_message[len + 1];
+	printf("HERE\n");
+
+	parser(new_message, message, len);
+	printf("HERE 2\n");
+
+	for (int i = 0; i < len + 1; i++)
+	{
+		union sigval value;
+		int pid = atoi(argv[1]);
+
+		value.sival_int = new_message[i];
+		printf("%i\n", new_message[i]);
+		if(sigqueue(pid, SIGUSR1, value) != 0)
+			printf("Message failed\n");
+		sleep(1);
+	}
+
 	return 0;
 }
--- server.c
+++ server.c
@@ -2,21 +2,50 @@
 #include <unistd.h>
 #include <stdio.h>
 
+int buffer[251];
 
 void startup()
 {
 	printf("Miniteams starting...\n");
 	printf("My PID is %i\n", getpid());
 	printf("Waiting for new messages\n");
+	buffer[250] = -1;
 }
 
+void parser(int len)
+{
+	printf("HERE\n");
+	printf("%i\n", len);
+	int i = 0;
+	char message[len];
+	for (int i = 0; i < len; ++i)
+	{
+		message[i] = buffer[i];
+		printf("%c\n", message[i]);
+	}
 
+	printf("%s\n", message);
+}
 
 void sig_handler(int sig, siginfo_t* info, void* vp)
 {
 	printf("received: %i\n", info->si_value.sival_int);
-}
 
+	int character = info->si_value.sival_int;
+
+	if (*buffer != -1)
+		if (character != '\0')
+		{
+			*buffer = character;
+			printf("%i\n", *buffer);
+			buffer+sizeof(int);
+		}
+		else
+		{
+			parser(*(buffer-(2 *sizeof(int))));
+			buffer - (*(buffer) + 1) * sizeof(int);
+		}
+}
 
 int main(int argc, char const *argv[])
 {
@@ -26,7 +55,6 @@ int main(int argc, char const *argv[])
 	sa.sa_sigaction = sig_handler;
 
 	sigaction(SIGUSR1, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
 
 	while(1)
 	{
